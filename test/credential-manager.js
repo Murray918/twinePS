d9a6849da022eb486c8518ee235a4f77b851f1e1
Andrew Murray <darbyguitars@gmail.com>
2018-09-12 21:23:00 -0500

    added a test using mocha and chai to make sure that the prompt is called once if there are not any apikeys or secrets

--- test/credential-manager.js
+++ test/credential-manager.js
@@ -1,3 +1,25 @@
-const chai = require('cahi')
+const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
+const inquirer = require('inquirer')
+const CredentialManager = require('../lib/credential-manager')
+
+describe('a credential manger', () => {
+  var creds
+  before(() => {
+    creds = new CredentialManager('twine-test')
+  })
+  context('with no existing credentials', () => {
+    it('should prompt the user', async () => {
+      sinon.stub(inquirer, 'prompt').resolves({ key: 'Party On', secret: 'Wayne' })
+      let [key, secret] = await creds.getKeyAndSecret()
+      expect(key).to.equal('Party On')
+      expect(secret).to.equal('Wayne')
+      inquirer.prompt.restore()
+    })
+  })
+  after(() => {
+    creds.conf.delete('apiKey')
+    creds.conf.delete('apiSecret')
+  })
+})
