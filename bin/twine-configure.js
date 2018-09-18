0c1e322ef9ea6f9c25358213b5df6d8bd6040def
Andrew Murray <darbyguitars@gmail.com>
2018-09-18 13:19:36 -0500

    created a error handler in the utils module to handle our catches

--- bin/twine-configure.js
+++ bin/twine-configure.js
@@ -1,6 +1,7 @@
 const program = require('commander')
 const pkg = require('../package.json')
 const configure = require('../commands/configure')
+const util = require('../lib/util')
 
 program
   .version(pkg.version)
@@ -8,16 +9,16 @@ program
 program
   .command('consumer')
   .description('Add a Twitter API key and secret')
-  .action(async () => {
-    await configure.consumer(pkg.name)
-  })
+  .action(() => configure
+    .consumer(util.extractName(pkg.name))
+    .catch(util.handleError))
 
 program
   .command('account')
-  .description('Authorize access to a twitter account')
-  .action(async () => {
-    await configure.account(pkg.name)
-  })
+  .description('Authorize access to a Twitter account')
+  .action(() => configure
+    .account(util.extractName(pkg.name))
+    .catch(util.handleError))
 
 program
   .parse(process.argv)
