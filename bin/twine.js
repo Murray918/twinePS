e538cc63362766ff846ab7086955f58d340b3e40
Andrew Murray <darbyguitars@gmail.com>
2018-09-19 20:09:01 -0500

    404 again going to switch to api-testing

--- bin/twine.js
+++ bin/twine.js
@@ -6,6 +6,7 @@ const pkg = require('../package.json')
 program
   .version(pkg.version)
   .command('configure', 'configure Twitter-related credentials')
+  .command('lookup', 'lookup things on twitter')
   .parse(process.argv)
 
 if (!process.argv.slice(2).length) {
